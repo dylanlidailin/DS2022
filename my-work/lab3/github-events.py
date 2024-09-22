e75d660905bc98bb636331e262e80816841d849d
Dailin Li <165076853+dylanlidailin@users.noreply.github.com>
2024-09-22 14:26:29 -0400

    Pruned some error documents

--- my-work/lab3/github-events.py
+++ my-work/lab3/github-events.py
@@ -1,9 +1,17 @@
-#!/bin/bash/python3
+#!/usr/local/bin/python3
 
 import os
 import json
 import requests
 
 GHUSER = os.getenv('GITHUB_USER')
-
 print(GHUSER)
+
+url = 'https://api.github.com/users/{0}/events'.format(GHUSER)
+print(url)
+
+r = json.loads(requests.get(url).text)
+
+for x in r[:5]:
+  event = x['type'] + ' :: ' + x['repo']['name']
+  print(event)
